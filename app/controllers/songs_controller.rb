28e9db463d68ae8a51ceb601e002a71b17461a0e
Hoon Lee <yl.hoony@gmail.com>
2018-04-13 22:17:02 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    SongsWorker.
+    SongsWorker.perform_async(params["file"].path)
     CSV.foreach(params["file"].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
     end
